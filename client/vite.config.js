6ed555e2ad844eed21789a78aff415cb4aa83991
Elena Erganyan <elena-erganyan@rambler.ru>
2023-11-16 17:13:59 +0300

    Some changes in configs

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,12 +9,15 @@ export default defineConfig({
     manifest: true,
     rollupOptions: {
       // overwrite default .html entry
-      input: './src/main.jsx',
+      input: "./src/main.jsx",
+      output: {
+        entryFileNames: "assets/[name]_v1.js"
+      },
     },
   },
   server: {
     proxy: {
-      '/api': 'http://localhost:3000',
+      "/api": "http://localhost:3000",
       }
     },
 });
